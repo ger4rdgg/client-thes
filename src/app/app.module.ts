04f7d1b63d8508a7e251b6675aa1181ca5c5a1db
Gerard <gerardgg02@gmail.com>
2021-02-01 16:15:20 +0100

    creacio de email

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,29 @@ import { AppComponent } from './app.component';
 import { EmailRequestsComponent } from './email/email-requests/email-requests.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavbarComponent } from './navbar/navbar.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import { EmailCreateComponent } from './email/email-create/email-create.component';
+import {ExternalConfigurationService} from './external-configuration-service';
+import {NgxHalClientModule} from '@lagoshny/ngx-hal-client';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     EmailRequestsComponent,
-    NavbarComponent
+    NavbarComponent,
+    EmailCreateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgxHalClientModule.forRoot(),
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    { provide: 'ExternalConfigurationService', useClass: ExternalConfigurationService },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
